411897e4e0753c5e00eb24e23c63d9ca3c4abf0e
Chavalit Koweerawong <chavalit.kow@gmail.com>
2021-03-22 02:52:50 +0700

    KNN

--- predict/mydynamo.py
+++ predict/mydynamo.py
@@ -3,6 +3,9 @@ import dynamo as dyn
 import pandas as pd
 import numpy as np
 import anndata
+from sklearn.neighbors import KDTree
+from sklearn.metrics import mean_squared_error
+
 
 
 def config() :
@@ -29,6 +32,8 @@ def removeOutlier(X_list):
     return X_list_new
 
 def removeOutlierObservation(a,b, obs):
+    # DEPLICATED
+    
     intersect_index = list( set(list(a.index)) & set(list(b.index)) & set(list(obs.index)) )
     intersect_index.sort()
     intersect_index
@@ -39,6 +44,8 @@ def removeOutlierObservation(a,b, obs):
     return [a,b,obs]
 
 def removeOutlierObservationExtra(a, obs):
+    # DEPLICATED
+    
     intersect_index = list( set(list(a.index)) & set(list(obs.index)) )
     intersect_index.sort()
     intersect_index
@@ -62,6 +69,7 @@ def createAnnotationDataForVectorField(X_list, obs_list, var) :
     # print("888")
 
 def createAnnotationData(a,b, obs, var, filter=True) :
+    # DEPLICATED
     # intersect_index = list( set(list(a.index)) & set(list(b.index)) & set(list(obs.index)) )
     # intersect_index.sort()
     # intersect_index
@@ -92,6 +100,8 @@ def dynamoProcess(adata) :
     return adata
 
 def dynamoProcess2(adata) :
+    # DEPLICATED
+    
     # print("555")
     # print("hello")
     dyn.pp.recipe_monocle(adata)
@@ -158,6 +168,27 @@ def concatDataFrameExtra(dfs):
     # df_piece = df_piece.set_index(['key'])
     return df_piece
 
+def createVectorFieldForEvaluation(adata) :    
+    print("Create Vector Field ...")
+
+    v = np.split(adata.obsm['X_umap'], 3)
+    # velocity_umap = v[1] - v[0]
+    velocity_umap = adata.obsm['X_umap'] - np.concatenate((v[0], v[0], v[2]), axis=0)
+
+    # PREPARE
+    adata3 = adata.copy()
+    # adata3.obsm['X_umap'] = v[0]
+    adata3.obsm['velocity_umap'] = velocity_umap
+    # adata3.uns['neighbors']['indices'] = np.split(adata2.uns['neighbors']['indices'],2)[0]
+    # nrows = len(adata3.uns['neighbors']['indices'])
+    # for i in range(nrows) : 
+    #     # adata3.uns['neighbors']['indices'][i] = adata3.uns['neighbors']['indices'][i][adata3.uns['neighbors']['indices'][i] >= nrows]
+    #     for j in range(len(adata3.uns['neighbors']['indices'][i])-2,-1,-1) :
+    #         if adata3.uns['neighbors']['indices'][i][j] >= nrows : 
+    #             adata3.uns['neighbors']['indices'][i][j] = adata3.uns['neighbors']['indices'][i][j+1]
+
+    return adata3
+
 def createVectorField(adata) :    
     print("Prepare to Create Vector Field")
 
@@ -194,14 +225,37 @@ def createVectorField(adata) :
 
     return adata3
 
-def knnToVectorField(adata) :   
+def knnToVectorField(adata, k=30) :   
     print("KNN")
     adata4 = adata.copy()
-    velocity_umap = adata.obsm['velocity_umap']
-    neighbors = adata.uns['neighbors']['indices']
-    for indices in adata.uns['neighbors']['indices']:
-        i = indices[0]
-        adata4.obsm['velocity_umap'][i] = averageVectorField(velocity_umap,indices)
+    # KNN
+    # X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    X = adata4.obsm['X_umap']
+    kdt = KDTree(X, leaf_size=30, metric='euclidean')
+    adata4.uns['neighbors']['indices'] = kdt.query(X, k=len(X), return_distance=False)
+
+    # adata4 = adata.copy()
+    velocity_umap = adata4.obsm['velocity_umap']
+    neighbors = adata4.uns['neighbors']['indices']
+    
+    # FILTER CURRENT ONLY + LIMIT with K 
+    new_neighbors = []
+    nrows = len(neighbors)
+    # k = 60
+    # GENERATE NEIGHBOR
+    for i in range(nrows):
+        indices = neighbors[i]
+        temp = neighbors[i][ np.logical_and(indices >= nrows/3 , indices < nrows/3*2) ]
+        new_neighbors.append( temp[:k] )
+    #     adata4.obsm['velocity_umap'][i] = averageVectorField(velocity_umap,indices)
+        # UPDATE velocity_umap
+        if i >= nrows/3 and i < nrows/3*2 :     
+            adata4.obsm['velocity_umap'][i] = averageVectorField(velocity_umap, np.array(new_neighbors) )
+        else :
+            adata4.obsm['velocity_umap'][i] = np.array([0,0])
+    # UPDATE NEIGHBOR
+    adata4.uns['neighbors']['indices'] = np.array(new_neighbors)
+    
     return adata4
 
 def averageVectorField(data,indices ):
@@ -209,3 +263,21 @@ def averageVectorField(data,indices ):
     filter_v = np.take(data, indices, axis=0)    
     return  np.average(filter_v,axis=0)
 
+def evaluate(adata) :
+    X = adata.obsm['X_umap']
+    V = adata.obsm['velocity_umap']
+    
+    Xs = np.split(X,3)
+    Vs = np.split(V,3)
+
+    #[0] : HISTORY
+    #[1] : PRESENT
+    #[2] : FUTURE
+    mean_squared_errors = []
+    X_predict = []    
+    X_true = []
+    for i in range(len(Xs[1])) :
+        X_predict.append(Xs[1][i] + Vs[1][i])
+        X_true.append(Xs[2][i])
+    return mean_squared_error(X_true, X_predict)
+        
